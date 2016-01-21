3e3ac3801cbf308f5cbf89ebba4f5fa23d2cc0ca
Dan Delp <delpdk@gmail.com>
2016-01-20 19:12:05 -0800

    Finished Portfolio Class02 Assigment

--- portfolioprojectclass02/Scripts/hhArticle.js
+++ portfolioprojectclass02/Scripts/hhArticle.js
@@ -9,19 +9,22 @@ function hhSpot (opts) {
   this.distance = opts.distance;
   this.image = opts.image;
   this.yelpReviewsUrl = opts.yelpReviewsUrl;
+  this.body = opts.body;
 }
 
 hhSpot.prototype.toHtml = function() {
   var $newhhSpot = $('.template').clone();
-
+  $newhhSpot.attr('data-bar', this.title);
+  $newhhSpot.attr('data-distance', this.distance);
   $newhhSpot.find('h1').html(this.title);
   $newhhSpot.find('h2').html('HH Time: ' + this.hhTime);
-  $newhhSpot.find('h3').html('Distance from Code Fellows: ' + this.distance);
+  $newhhSpot.find('h3').html(this.distance);
   $newhhSpot.find('.location').html('Address: ' + this.location);
   $newhhSpot.find('.hhMenu').html(this.hhMenuUrl);
   $newhhSpot.find('.venueWebsite').html(this.venueWebsiteUrl);
   $newhhSpot.find('.image').html(this.image);
   $newhhSpot.find('.yelpReviewsUrl').html(this.yelpReviewsUrl);
+  $newhhSpot.find('.hh-body').html(this.body);
   $newhhSpot.removeClass('template');
   return $newhhSpot;
 }
--- portfolioprojectclass02/Scripts/hhRawData.js
+++ portfolioprojectclass02/Scripts/hhRawData.js
@@ -7,7 +7,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href= "http://republicseattle.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href= "http://www.yelp.com/biz/re-public-seattle-2"> Yelp Reviews </a>',
     distance:        '0.1',
-    image:           '<img src = "https://s-media-cache-ak0.pinimg.com/originals/3f/18/26/3f1826027e638ad0005a99dbffc908ab.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "https://s-media-cache-ak0.pinimg.com/originals/3f/18/26/3f1826027e638ad0005a99dbffc908ab.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> While it’s not a great idea to have a colon in a restaurant name—especially when people will have to say the word “colon” in order to explain it—this South Lake Union place is pretty great. The local/organic-oriented new American food is both interesting and really good. Definitely try the house-made pasta, and (if they have it) the crispy pig tail—like a mini corn dog, with the tail just a meltingly creamy-fatty memory of meat sealed inside a small tube of deep-fried goodness, served with a dab of whole-grain mustard vinaigrette and half a soft-boiled egg. </p>'
   },
 
   {
@@ -18,7 +19,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://bravehorsetavern.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/brave-horse-tavern-seattle"> Yelp Reviews </a>',
     distance:        '0.2',
-    image:           '<img src = "http://beermapping.com/maps/reviews/images/locations/PANASONIC_DMC_TZ5_P1060039.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "http://beermapping.com/maps/reviews/images/locations/PANASONIC_DMC_TZ5_P1060039.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> Tom Douglas, Seattle’s most famous restaurateur, brings his intelligent brand of comfort food to the burgeoning South Lake Union headquarters of Amazon, in a classic wood-rafters-and-vintage-signs beer hall above his Cuoco Italian restaurant. The deal is 26 tap beers (including a Brave Horse signature IPA brewed in SoDo) with burgers and fresh pretzels for ballast. Burgers are on the diminutive side, but crafted of Painted Hills chuck, ground in-house, and topped with apple wood bacon, perhaps, or fried pasilla chili, on terrific Dahlia bakery buns. Better yet are the malt-boiled, hearth-roasted pretzels—fresh, buttery, yeasty testimonials to the Douglas empire’s baking savvy—that you dip into toppings from cheddar-pimento to smoky peanut butter-bacon. The former warehouse is exuberant as spring break, with young adults playing darts and shuffleboard, and a crowd of dashing young tech titans oozing testosterone. (Bring the trolling ladyfriends.) </p>'
   },
 
   {
@@ -29,7 +31,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://www.13coins.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/13-coins-restaurant-seattle"> Yelp Reviews </a>',
     distance:        '0.3',
-    image:           '<img src = "http://s3-media3.fl.yelpcdn.com/bphoto/gslkLWYpWxQ30A1s532fkg/ls.jpg" height = "400px" width = "600px"/>' 
+    image:           '<img src = "http://s3-media3.fl.yelpcdn.com/bphoto/gslkLWYpWxQ30A1s532fkg/ls.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> The 13 Coins name is of Peruvian origin. The story goes that a poor young man loved and wished to marry a wealthy girl. Her father asked what he had to offer for his daughters hand in marriage. The young man reached into his pocket. He had only 13 coins, but assured the father he could pledge undying love, care and concern. The father was so touched, he gave his daughters hand and 13 Coins has come to symbolize unyielding love, care and concern. As evergreen as its Seattle surroundings, 13 Coins is a timeless Seattle institution where generations have settled in for an unforgettable experience since 1967. Famed for its high backed booths, swiveling captain’s chairs and an exhibition kitchen known for lively, counter-side exchanges, 13 Coins has been host to countless first dates and proposals, family and company gatherings, celebrity sightings, and many late (or early!) nights on the town with friends. Delicious food lining a dazzling 24-hour menu, engaging line chefs who banter playfully, and friendly, professional servers keep you coming back time and again. </p>'
   },
 
   {
@@ -40,7 +43,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://www.dukeschowderhouse.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/dukes-chowder-house-seattle-9"> Yelp Reviews </a>',
     distance:        '0.3',
-    image:           '<img src = "http://media-cdn.tripadvisor.com/media/photo-s/07/cd/27/88/duke-s-chowder-house.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "http://media-cdn.tripadvisor.com/media/photo-s/07/cd/27/88/duke-s-chowder-house.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> Duke’s is honored to have received the highest seafood restaurant rating in the State of Washington by Fish2Fork, a leading evaluator of worldwide seafood restaurants, garnering a 4.5 out of 5 (no higher rating in the world). Duke’s was also recently awarded a 100 percent sustainable seafood volume compliance rating from Smart Catch, a program designed to promote sustainability and raise consumer awareness regarding sustainable seafood options. At Duke’s, we are not interested in compromising quality in any way; we have relentlessly developed incredible sources that provide amazing quality. Almost all of the seafood we feature here at Duke’s comes from the Northwest and Alaska. We thank you for coming in and I personally want to welcome you to our restaurant. I think you will find that we care deeply about the food we serve and the enjoyment of our guests. </p>'
   },
 
   {
@@ -51,7 +55,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://www.feierabendseattle.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/feierabend-seattle"> Yelp Reviews </a>',
     distance:        '0.3',
-    image:           '<img src = "https://c1.staticflickr.com/5/4050/4481121882_baec5390ee_b.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "https://c1.staticflickr.com/5/4050/4481121882_baec5390ee_b.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> The third German pub by Chris Navarra, Feierabend opened its doors February 26, 2006 to serve German bier and food to South Lake Union.  The menu, by Chef Ryan Sullivan, is designed to be a marriage of German and Northwest cuisine.  Some of the menu items are traditional German fare, while others have been adapted for our Seattle palates.  We use both local purveyors and imported items to produce food we are proud to share.  All eighteen of our tap biers are imported from Germany.“Feierabend” means “quittin’ time” in German.  So, relax and enjoy Feierabend! </p> '
 
   },
 
@@ -63,7 +68,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://witnessbar.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/witness-seattle"> Yelp Reviews </a>',
     distance:        '0.7',
-    image:           '<img src = "http://3.bp.blogspot.com/-ogMVZgBPNc4/VIj5SltcfpI/AAAAAAAAN2s/4_fQuIZ58LE/s1600/witness-20130926-003.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "http://3.bp.blogspot.com/-ogMVZgBPNc4/VIj5SltcfpI/AAAAAAAAN2s/4_fQuIZ58LE/s1600/witness-20130926-003.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> Witness is a Southern-influenced craft cocktail bar that places emphasis on warm hospitality and premium-quality beverages.The food menu features Southern-inspired dishes that lean more toward “soul food” than “New Southern Cuisine.” Witness delivers charm, easy elegance, and down-home warmth. </p>'
   },
 
   {
@@ -74,7 +80,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://www.pineboxbar.com/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/the-pine-box-seattle"> Yelp Reviews </a>',
     distance:        '0.7',
-    image:           '<img src = "http://media.tumblr.com/tumblr_m4y15qEwvY1r6eqh1.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "http://media.tumblr.com/tumblr_m4y15qEwvY1r6eqh1.jpg" height = "400px" width = "600px"/>',
+    body:            '<p> Headed by Ian Roberts (a founder of Seattle Beer Week and former bar manager at Brouwer’s), the Pine Box enlivens its (gorgeous) mortuary setting with more than 30 beers on tap. The pizza is good, and the short rib—a Flintstones-sized block of meat served with tzatziki and a shiitake/Parmesan hand pie—is great. Mostly, the Pine Box works marvelously as a beer hall. It’s loud and lovely and a lot of fun. </p>'
   },
 
   {
@@ -85,7 +92,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://www.thenabobbar.com/about"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/the-great-nabob-seattle"> Yelp Reviews </a>',
     distance:        '0.6',
-    image:           '<img src = "https://c1.staticflickr.com/3/2446/3622798893_d38d68283c.jpg" height = "400px" width = "600px"/>'
+    image:           '<img src = "https://c1.staticflickr.com/3/2446/3622798893_d38d68283c.jpg" height = "400px" width = "600px"/>',
+    body:            "<p> The Nabob is a place to meet your friends and family to visit, to celebrate: the day, events both great and small or just being alive. A comfortable spot with atmosphere in combination with great service, top notch beverages and a safe haven. The term Nabob originates in India during the Muslim reign. Nabobs were governors in the Mogul's Empire. As the British influence broadened in India the Nabobs became men of great wealth and wisdom.</p>"
   },
 
   {
@@ -96,7 +104,8 @@ var hhRawData = [
     venueWebsiteUrl: '<a href = "http://www.nypbarandgrill.com/about-us/"> Venue Website </a>',
     yelpReviewsUrl:  '<a href = "http://www.yelp.com/biz/nyp-bar-and-grill-seattle?osq=new+york+pizza+bar+and+grill"> Yelp Reviews </a>',
     distance:        '0.5', 
-    image:           '<img src = "http://www.nypbarandgrill.com/wp-content/uploads/2015/08/logo.png" height = "400px" width = "600px"/>' 
+    image:           '<img src = "http://www.nypbarandgrill.com/wp-content/uploads/2015/08/logo.png" height = "400px" width = "600px"/>',
+    body:            '<p> All of our food is made in-house, from scratch. The pizza dough for our NYC pizza is made fresh daily with herbs, garlic and Bellingham’s own Boundary Bay Scotch Ale and is hand tossed to order. We make our classic red pizza sauce, marinara and alfredo in-house along with most of our signature dipping sauces. Fries are hand cut, in house and made from potatoes grown in Washington State. Your meal is made from honest and natural ingredients like nitrate-free, uncured pepperoni, from Zoe’s Meats and natural 100% hormone and antibiotic free beef for all burgers, raised in the Columbia Valley. We use only premium, hand crafted meats, that are almost all Washington State sourced and come in the most natural state available. We care about your experience and offer a dedicated gluten-free menu, a perfectly selected Northwest tap list for the beer obsessed, a great sports viewing environment or a family friendly experience. If we can make it, we happily will, don’t be afraid to ask! We have happy hour every day and an amazing weekend brunch. We always buy local. NYP is proud to use Macrina Bakery and Breadfarm Breads, Zoe’s Meats, Isernio’s Sausage, Draper Valley Farms and Hempler’s Bacon. We pride ourselves in sourcing many of our ingredients locally. We love to be a part of the craft brew movement and proudly “drink local” at NYP. </p>'
   }
 
 ];
