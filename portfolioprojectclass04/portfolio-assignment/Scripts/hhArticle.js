0945036f5d519fe0fd5650bb1650193f82b241eb
Dan Delp <delpdk@gmail.com>
2016-01-28 22:14:53 -0800

    Transferred files and worked on Portfolio assignment 4

---
+++ portfolioprojectclass04/portfolio-assignment/Scripts/hhArticle.js
@@ -0,0 +1,33 @@
+var hhSpots = [];
+
+function hhSpot (opts) {
+  this.title = opts.title;
+  this.hhTime = opts.hhTime;
+  this.location = opts.location;
+  this.hhMenuUrl = opts.hhMenuUrl;
+  this.venueWebsiteUrl = opts.venueWebsiteUrl;
+  this.distance = opts.distance;
+  this.image = opts.image;
+  this.yelpReviewsUrl = opts.yelpReviewsUrl;
+  this.body = opts.body;
+}
+
+hhSpot.prototype.toHtml = function() {
+  var newhhSpot = Handlebars.compile($('#hh-template').text());
+  this.body = marked(this.body);
+  return newhhSpot(this);
+};
+
+if (typeof rawData !== 'undefined') {
+  hhRawData.sort(function(a,b) {
+    return (new Date(a.distance)) - (new Date(b.distance));
+  });
+
+  hhRawData.forEach(function(ele) {
+    hhSpots.push(new hhSpot(ele));
+  })
+}
+
+hhSpots.forEach(function(a){
+  $('#hhSpots').append(a.toHtml())
+});
